21a35ab26082304f929ee80580ad98e3c5ed3ef7
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-05-23 22:32:19 -0700

    added appointment scheduler component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import AppointmentScheduler  from './components/AppointmentScheduler';
 import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/appointment-scheduler" Component={AppointmentScheduler} />
         <Route path="/dashboard" Component={Dashboard} />
         <Route path="/"  Component={Dashboard} />
       </Switch>
---
+++ src/components/AppointmentScheduler.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppointmentScheduler: React.FC = () => {
+  return (
+    <div>
+      <h1>Appointment Scheduler</h1>
+      <p>Manage and schedule appointments here.</p>
+    </div>
+  );
+}
+
+export default AppointmentScheduler;
