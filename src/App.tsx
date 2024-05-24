9f51f73087bcdbf32c763d45cc19f42b1ae23502
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-05-23 22:34:08 -0700

    added login component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppointmentScheduler  from './components/AppointmentScheduler';
 import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/login" Component={Login} />
         <Route path="/appointment-scheduler" Component={AppointmentScheduler} />
         <Route path="/dashboard" Component={Dashboard} />
         <Route path="/"  Component={Dashboard} />
---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Authentication page for different user roles.</p>
+    </div>
+  );
+}
+
+export default Login;
